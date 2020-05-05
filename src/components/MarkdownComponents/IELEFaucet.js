7efe77beb3cb1602d93b970368704e9c39177ea0
Lewis Nelson <lewis.nelson@iohk.io>
2020-05-05 15:46:49 +0100

    Added IELE faucet component

---
+++ src/components/MarkdownComponents/IELEFaucet.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default () => (
+  <p><strong>IELE Faucet - TODO</strong></p>
+)
--- src/components/MarkdownComponents/index.js
+++ src/components/MarkdownComponents/index.js
@@ -1,12 +1,14 @@
-import ByronFaucet from './ByronFaucet'
 import ByronDaedalusDownloaders from './ByronDaedalusDownloaders'
+import ByronFaucet from './ByronFaucet'
+import IELEFaucet from './IELEFaucet'
 import ITNFaucet from './ITNFaucet'
 import KEVMFaucet from './KEVMFaucet'
 import OtherAvailableTestnets from './OtherAvailableTestnets'
 
 export default {
-  ByronFaucet,
   ByronDaedalusDownloaders,
+  ByronFaucet,
+  IELEFaucet,
   ITNFaucet,
   KEVMFaucet,
   OtherAvailableTestnets
