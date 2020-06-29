efb3b0de1e9938b659faafd57a7ac30d08947e8d
Lewis Nelson <lewis.nelson@iohk.io>
2020-06-29 14:45:44 +0100

    Added wallet downloaders for Shelly

--- src/components/MarkdownComponents/ShelleyDaedalusDownloaders.js
+++ src/components/MarkdownComponents/ShelleyDaedalusDownloaders.js
@@ -3,7 +3,7 @@ import WalletDownloaders from '../WalletDownloaders'
 
 export default () => (
   <WalletDownloaders
-    gaCategory='byron_daedalus_downloaders'
+    gaCategory='shelley_daedalus_downloaders'
     settingsEndpoint='https://updates-cardano-testnet.s3.amazonaws.com/daedalus-latest-version.json'
   />
 )
