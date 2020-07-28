cb86a6d9ae8fde2a27bf0b0011f7b5b051b1e976
jcsmillie <jonnysmillie@jonnysmillie.co.uk>
2020-07-28 20:27:17 +0100

    remove byron, rename shelley cardano in most places...

--- src/pages/index.js
+++ src/pages/index.js
@@ -148,19 +148,13 @@ export default () => (
               <li>
                 <Link href='/shelley/overview/' tracking={{ category: 'home_page', label: 'shelley_haskell_cta' }}>
                   <img src='/images/shelley.png' alt='Shelley' />
-                  <span>Shelley</span>
+                  <span>Cardano</span>
                 </Link>
               </li>
               <li>
                 <Link href='/itn/overview/' tracking={{ category: 'home_page', label: 'itn_cta' }}>
                   <img src='/images/shelley.png' alt='Shelley' />
                   <span>ITN</span>
-                </Link>
-              </li>
-              <li>
-                <Link href='/byron/overview/' tracking={{ category: 'home_page', label: 'byron_cta' }}>
-                  <img src='/images/byron.png' alt='Byron' />
-                  <span>Byron</span>
                 </Link>
               </li>
               <li>
