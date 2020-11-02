48d0a31db3025769dbd9e93c69ebb63160a0d326
George Clark <clarkgeorge76@gmail.com>
2020-11-02 16:39:04 +0000

    add table page and style

---
+++ resources/content/articles/en/2020-10-28_11-00-00_active-testnets-en.md
@@ -0,0 +1,17 @@
+---
+title: Active Testnets
+description: Acttive Testnet matrix
+parent: 2020-05-04_04-00-00_native-tokens
+order: 3
+last_updated: "2020-05-01T09:00:00+01:00"
+hasNoChildContent: true
+
+---
+## Active Testnets
+
+| Chain ID |       Name       | Short Name | Chain | Network | Network ID |
+|:--------:|:----------------:|:----------:|:-----:|:-------:|:----------:|
+|     1    | Ethereum mainnet |     eth    |  ETH  | mainnet |      1     |
+|    100   |    xDAI chain    |    xdai    |  XDAI | mainnet |     100    |
+|    101   |     EtherInc     |     eti    |  ETI  | mainnet |      1     |
+
--- src/components/Style.js
+++ src/components/Style.js
@@ -13,4 +13,48 @@ export default createGlobalStyle`
       }
     }
   }
+
+  table {
+      /* Remove spacing between table cells (from Normalize.css) */
+      border-collapse: collapse;
+      border-spacing: 0;
+      empty-cells: show;
+      border: 1px solid #cbcbcb;
+      font-size:89%;
+  }
+
+  table caption {
+      color: #000;
+      font: italic 85%/1 arial, sans-serif;
+      padding: 1em 0;
+      text-align: center;
+  }
+
+  table td,
+  table th {
+      border-left: 1px solid #cbcbcb;/*  inner column border */
+      border-width: 0 0 0 1px;
+      font-size: inherit;
+      margin: 0;
+      overflow: visible; /*to make ths where the title is really long work*/
+      padding: 0.5em 1em; /* cell padding */
+      line-height: 1;
+      white-space: nowrap;
+  }
+
+  table thead {
+      background-color: #e0e0e0;
+      color: #000;
+      text-align: left;
+      vertical-align: bottom;
+  }
+
+  /*
+  striping:
+    even - #fff (white)
+    odd  - #f2f2f2 (light gray)
+  */
+  table td {
+      background-color: transparent;
+  }
 `
