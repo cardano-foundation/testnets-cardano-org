8f5864eca4e1427ade26396bb543451ec84f6b69
elviejo79 <agarciafdz@gmail.com>
2020-12-16 15:55:03 -0600

    as suggest by @jonnysmillie, the port doesn't change only the domain

--- src/components/MarkdownComponents/KEVMFaucet.js
+++ src/components/MarkdownComponents/KEVMFaucet.js
@@ -3,7 +3,7 @@ import Faucet from '../Faucet'
 
 export default () => (
   <Faucet
-    getEndpoint={({ address }) => `https://faucet.kevm.dev.cardano.org/faucet?address=${address}`}
+    getEndpoint={({ address }) => `https://faucet.kevm.dev.cardano.org:8099/faucet?address=${address}`}
     hasApiKey={false}
     getTransactionURL={({ txid }) => `https://explorer.kevm.dev.cardano.org/tx/${txid}`}
   />
