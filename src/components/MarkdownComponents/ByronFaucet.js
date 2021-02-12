1eda2be4405240855b7952138c27691ce44ca207
George Clark <clarkgeorge76@gmail.com>
2021-02-12 13:28:35 +0000

    addition of input for megacoin native tokens

--- src/components/MarkdownComponents/ByronFaucet.js
+++ src/components/MarkdownComponents/ByronFaucet.js
@@ -3,12 +3,13 @@ import Faucet from '../Faucet'
 
 export default () => (
   <Faucet
-    getEndpoint={({ address, apiKey, reCaptchaResponse }) => `https://faucet2.cardano-testnet.iohkdev.io/send-money/${address}?apiKey=${apiKey}&g-recaptcha-response=${reCaptchaResponse}`}
+    getEndpoint={({ address, apiKey, reCaptchaResponse }) => `https://faucet.cardano-testnet.iohkdev.io/send-money/${address}?apiKey=${apiKey}&g-recaptcha-response=${reCaptchaResponse}`}
     hasApiKey
     getTransactionURL={({ txid }) => `https://cardano-explorer.cardano-testnet.iohkdev.io/tx/${txid}`}
     reCaptcha={{
       version: 2,
       sitekey: '6LeUZ64ZAAAAAMHWlSUqsT2bt8HS5fZXngoyeMRB'
     }}
+    getNativeAssetEndpoint={({ address, reCaptchaResponse }) => `https://faucet.cardano-testnet.iohkdev.io/send-money/${address}?asset=${`6b8d07d69639e9413dd637a1a815a7323c69c86abbafb66dbfdb1aa7`}&g-recaptcha-response=${reCaptchaResponse}`}
   />
 )
