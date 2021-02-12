c9cb383d46b6323ebd7cd71bf47c0aec25bd794e
George Clark <clarkgeorge76@gmail.com>
2021-02-12 14:10:48 +0000

    addition of input for megacoin native tokens

--- src/components/Faucet.js
+++ src/components/Faucet.js
@@ -89,23 +89,19 @@ const FaucetInner = ({ content, getEndpoint, hasApiKey, getTransactionURL, reCap
 
   const onSubmit = async (e) => {
     e.preventDefault()
-    const endpointParams = { address: values.address, apiKey: values.apiKey }
-    const url = isNativeAssetReq === 'Ada' ? getEndpoint(endpointParams) : getNativeAssetEndpoint(endpointParams)
-
     const newErrors = {}
     if (!values.reCaptcha) newErrors.reCaptcha = content.faucet_content.please_complete_recaptcha
     if (Object.keys(newErrors).length > 0) {
       setErrors(newErrors)
       return
     }
-
     setErrors(DEFAULT_ERRORS)
     setServerError('')
     setStatus(statuses.loading)
     try {
-      // const endpointParams = { address: values.address, apiKey: values.apiKey }
+      const endpointParams = { address: values.address, apiKey: values.apiKey }
       if (reCaptcha) endpointParams.reCaptchaResponse = values.reCaptcha
-      // const url = isNativeAssetReq === 'Ada' ? getEndpoint(endpointParams) : getNativeAssetEndpoint(endpointParams)
+      const url = isNativeAssetReq === 'Ada' ? getEndpoint(endpointParams) : getNativeAssetEndpoint(endpointParams)
       const result = await fetch(url, { method: 'POST' })
       const jsonResult = await result.json()
       if (result.status === 200 && jsonResult.success === false) {
