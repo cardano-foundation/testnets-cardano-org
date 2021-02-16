2144c3dd3eeb12226788453125acd3f5b7fd5237
George Clark <clarkgeorge76@gmail.com>
2021-02-16 15:11:30 +0000

    add amount to native token response

--- src/components/Faucet.js
+++ src/components/Faucet.js
@@ -88,7 +88,7 @@ const FaucetInner = ({ content, getEndpoint, hasApiKey, getTransactionURL, reCap
         return content.faucet_content.funds
       }
     } else {
-      return `your ${nativeToken}`
+      return `your ${result.amount} ${nativeToken}`
     }
   }
 
