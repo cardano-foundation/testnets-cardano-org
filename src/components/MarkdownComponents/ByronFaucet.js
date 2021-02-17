4f7b2dcea8694032d8f72e7392cef267793e99a7
George Clark <clarkgeorge76@gmail.com>
2021-02-17 13:05:00 +0000

    change explorer to new url

--- src/components/MarkdownComponents/ByronFaucet.js
+++ src/components/MarkdownComponents/ByronFaucet.js
@@ -5,7 +5,7 @@ export default () => (
   <Faucet
     getEndpoint={({ address, apiKey, reCaptchaResponse }) => `https://faucet.cardano-testnet.iohkdev.io/send-money/${address}?apiKey=${apiKey}&g-recaptcha-response=${reCaptchaResponse}`}
     hasApiKey
-    getTransactionURL={({ txid }) => `https://cardano-explorer.cardano-testnet.iohkdev.io/tx/${txid}`}
+    getTransactionURL={({ txid }) => `https://explorer.cardano-testnet.iohkdev.io/tx/${txid}`}
     reCaptcha={{
       version: 2,
       sitekey: '6LeUZ64ZAAAAAMHWlSUqsT2bt8HS5fZXngoyeMRB'
